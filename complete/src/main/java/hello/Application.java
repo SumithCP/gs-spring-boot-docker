d62d91d62bce7e4f49c3e02f776a80f0cd8fd072
SumithCP <56859063+SumithCP@users.noreply.github.com>
2020-09-21 09:30:01 +0530

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "My Test Website";
+		return "My Test Website 1";
 	}
 
 	public static void main(String[] args) {
